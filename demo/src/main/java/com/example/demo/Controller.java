676bc16d430e3bd73bcdb22bded2fc1203564432
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-11-04 11:02:41 -0300

    Criar injeção de dependência

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/investimento")
 public class Controller {
+
+    @Autowired
+    private Service service;
+
+
+
 }
