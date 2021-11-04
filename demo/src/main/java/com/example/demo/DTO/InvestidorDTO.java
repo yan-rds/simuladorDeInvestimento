19c3f2c57993b9974f0febe8359713e66301d113
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-11-04 10:59:38 -0300

    Moldar classe investidorDTO

--- demo/src/main/java/com/example/demo/DTO/InvestidorDTO.java
+++ demo/src/main/java/com/example/demo/DTO/InvestidorDTO.java
@@ -1,4 +1,72 @@
 package com.example.demo.DTO;
 
+import com.example.demo.Risco;
+
 public class InvestidorDTO {
+    private String email;
+    private String nome;
+    private String cpf;
+    private double valorPrevisto;
+    private int periodoDeAplicacao;
+    private Risco risco;
+
+    public InvestidorDTO(String email, String nome, String cpf, double valorPrevisto, int periodoDeAplicacao, Risco risco) {
+        this.email = email;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.valorPrevisto = valorPrevisto;
+        this.periodoDeAplicacao = periodoDeAplicacao;
+        this.risco = risco;
+    }
+
+    public InvestidorDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorPrevisto() {
+        return valorPrevisto;
+    }
+
+    public void setValorPrevisto(double valorPrevisto) {
+        this.valorPrevisto = valorPrevisto;
+    }
+
+    public int getPeriodoDeAplicacao() {
+        return periodoDeAplicacao;
+    }
+
+    public void setPeriodoDeAplicacao(int periodoDeAplicacao) {
+        this.periodoDeAplicacao = periodoDeAplicacao;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
 }
